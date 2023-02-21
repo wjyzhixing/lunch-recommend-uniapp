ea744284922b12f06d8054d0caac34e9b239abed
wangjingyu <0027017083@dayu.work>
2023-02-21 16:34:45 +0800

    修改api key

--- App.vue
+++ App.vue
@@ -1,33 +1,33 @@
 <script>
 export default {
-	globalData: {
-		    token: '',
-			user: '',
-			// host: 'http://43.143.38.230:7001',
-			host: '/api',
-	},
-	onLaunch: function() {
-		console.log('App Launch');
+  globalData: {
+    token: '',
+    user: '',
+    host: 'http://43.143.38.230:7001',
+    // host: '/api',
+  },
+  onLaunch: function () {
+    console.log('App Launch');
 
-		setTimeout(() => {
-			// uni.setTabBarBadge({
-			// 	index: 1,
-			// 	text: '31'
-			// });
-			// uni.showTabBarRedDot({
-			// 	index: 3
-			// });
-		}, 1000);
-	},
-	onShow: function() {	
-		console.log('App Show');
-	},
-	onHide: function() {
-		console.log('App Hide');
-	}
+    setTimeout(() => {
+      // uni.setTabBarBadge({
+      // 	index: 1,
+      // 	text: '31'
+      // });
+      // uni.showTabBarRedDot({
+      // 	index: 3
+      // });
+    }, 1000);
+  },
+  onShow: function () {
+    console.log('App Show');
+  },
+  onHide: function () {
+    console.log('App Hide');
+  },
 };
 </script>
 
 <style>
-/*每个页面公共css */ 
+/*每个页面公共css */
 </style>
--- pages/tabbar/chat/chat.vue
+++ pages/tabbar/chat/chat.vue
@@ -1,91 +1,100 @@
 <template>
-	<view>
-		<view class="contentTop">
-		    <text class="eatWhat">您的问题：</text>
-		    <uni-easyinput v-model="eatWhat" placeholder="请输入您的问题" />
-		</view>
-		<view>
-			<button class="btn" type="primary" @click="submit()">查询</button>
-		
-		</view>
-		<view class="contentTT">{{ result }}</view>
-	</view>
+  <view>
+    <view class="contentTop">
+      <text class="eatWhat">您的问题：</text>
+      <uni-easyinput v-model="eatWhat" placeholder="请输入您的问题" />
+    </view>
+    <view class="contentTop">
+      <text class="eatWhat">api key：</text>
+      <uni-easyinput v-model="apiKey" placeholder="请输入您的api key" />
+    </view>
+    <view>
+      <button class="btn" type="primary" @click="submit()" :loading="loading">
+        查询
+      </button>
+    </view>
+    <view class="contentTT">{{ result }}</view>
+  </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				eatWhat: '',
-				result: ''
-			}
-		},
-		methods: {
-			submit() {
-				const host = getApp().globalData.host;
-			
-				uni.request({
-					url: host + "/chatgpt",
-					method: 'POST',
-					data: {
-						text: this.eatWhat,
-					},
-					header: {
-						"Content-Type": "application/x-www-form-urlencoded",
-					},
-					success: (res) => {
-						console.log(res)
-						if (res?.data?.code !== 0 && res?.data) {
-							uni.showToast({
-								title: `${res?.data?.message}`,
-								icon: 'none'
-							})
-						} else {
-							this.result = res?.data?.data?.msg
-						}
-					},
-					fall: e => {
-						console.log(e)
-					}
-				})
-			}
-		}
-	}
+export default {
+  data() {
+    return {
+      eatWhat: '',
+      result: '',
+      apiKey: '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',
+      loading: false,
+    };
+  },
+  methods: {
+    submit() {
+      const host = getApp().globalData.host;
+      this.loading = true;
+      uni.request({
+        url: host + '/chatgpt',
+        method: 'POST',
+        data: {
+          text: this.eatWhat,
+          apiKey: this.apiKey,
+        },
+        header: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        success: (res) => {
+          this.loading = false;
+          console.log(res);
+          if (res?.data?.code !== 0 && res?.data) {
+            uni.showToast({
+              title: `${res?.data?.message}`,
+              icon: 'none',
+            });
+          } else {
+            this.result = res?.data?.data?.msg;
+          }
+        },
+        fall: (e) => {
+          console.log(e);
+        },
+      });
+    },
+  },
+};
 </script>
 
 <style lang="less">
-    .openBut{
-        margin: 20upx auto;
-        background-color: #007AFF;
-        color: #FFF;
-        width: 90%;
-        // height: 60upx;
-        // line-height: 60upx;
-        text-align: center;
-        border-radius: 10upx;
-        // position: fixed;
-        // bottom: 80upx;
-        text-align: center;
-    }
-	.btn {
-		margin: 0 30upx;
-	}
-    .contentTop {
-        margin: 30upx;
-        display: flex;
-        align-content: center;
+.openBut {
+  margin: 20upx auto;
+  background-color: #007aff;
+  color: #fff;
+  width: 90%;
+  // height: 60upx;
+  // line-height: 60upx;
+  text-align: center;
+  border-radius: 10upx;
+  // position: fixed;
+  // bottom: 80upx;
+  text-align: center;
+}
+.btn {
+  margin: 0 30upx;
+}
+.contentTop {
+  margin: 30upx;
+  display: flex;
+  align-content: center;
 
-        .eatWhat {
-            display: flex;
-            align-content: center;
-            margin: auto;
-        }
-    }
-    .contentTT {
-        margin: 30upx 30upx;
-        display: flex;
-        align-content: center;
-        justify-content: center;
-        font-size: 40upx;
-    }
+  .eatWhat {
+    display: flex;
+    align-content: center;
+    margin: auto;
+  }
+}
+.contentTT {
+  margin: 30upx 30upx;
+  display: flex;
+  align-content: center;
+  justify-content: center;
+  font-size: 40upx;
+}
 </style>
